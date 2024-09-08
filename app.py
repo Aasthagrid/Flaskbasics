8d8586d35d4ddf48b3d53c76f664436be53f8585
Aastha <aasthasonu28@gmail.com>
2024-09-08 10:28:09 +0530

    dynamic url done

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 # Importing flask module in the project is mandatory
 # An object of Flask class is our WSGI application.
 from flask import Flask
-
 # Flask constructor takes the name of 
 # current module (__name__) as argument.
 app = Flask(__name__)
@@ -14,9 +13,17 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World'
 
+@app.route('/results/<int:score>')
+def success(score):
+    result=""
+    if score<50:
+        result="fail"
+    else:
+        result="success"
+    return result
+
 # main driver function
 if __name__ == '__main__':
-
     # run() method of Flask class runs the application 
     # on the local development server.
-    app.run()
+    app.run(debug=True)
